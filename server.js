32f5cafe9ffc20655cdd9890fd3f9c5ac5266d4f
Brian Alegre <brialegre@yahoo.com>
2022-06-01 22:06:50 -0700

    Added GET and POST for API calls

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
-const uuid = require('uuid')
+const uuid = require('./helpers/uuid')
 const app = express();
 
 // Middleware for parsing JSON and URLencoded data
@@ -14,15 +14,77 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
 
+// Variables
+const notesData = require('./db/db.json');
+const req = require('express/lib/request');
+
 // GET Route for notes.html
-app.get(`/notes`, (req, res) =>
+app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
-// GET Route for index.html
-app.get(`*`, (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET Route for /api/notes
+app.get('/api/notes', (req, res) =>
+    res.json(notesData)
 )
 
+// POST Route for /api/notes
+app.post('/api/notes', (req, res) => {
+    // Log POST received
+    console.log(`${req.method} request recieved to add to notes`)
 
+    // Read current saved notes
+    const currentNotes = fs.readFileSync(path.join(process.cwd(), '/db/db.json'))
 
+    // Deconstruct
+    const { title, text } = req.body;
+
+    // Check if all keys are populated
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        }
+
+        // Obtain existing reviews
+        fs.readFileSync('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                // Convert string into JSON object
+                const parsedNotes = JSON.parse(data);
+
+                // Add a new review
+                parsedNotes.push(newNotes);
+
+                // Write updated reviews back to the file
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated Notes!')
+                );
+            }
+        });
+
+        // Response Status
+        const response = {
+            status: 'Succcess',
+            body: newNote,
+        }
+
+        // Send response status
+        res.status(201).json(response)
+    } else {
+        res.status(500).json('Error in POST')
+    }
+})
+
+// GET Route for index.html
+// Needs to be last
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+)
