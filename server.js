e58480e338b2ff9afb382a890cab2615548b33c6
Brian Alegre <brialegre@yahoo.com>
2022-06-02 18:21:56 -0700

    Changed read/write file to async

--- server.js
+++ server.js
@@ -48,27 +48,19 @@ app.post('/api/notes', (req, res) => {
 
 
         // Read current saved notes
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                // Convert string into JSON object
-                const parsedNotes = JSON.parse(data);
+        const readNotes = fs.readFileSync('./db/db.json', 'utf8')
 
-                // Add a new review
-                parsedNotes.push(newNote);
-                console.log(`Hello this is brian`)
-                // Write new notes back to the file
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parsedNotes, null, 4),
-                    (writeErr) =>
-                        writeErr
-                            ? console.error(writeErr)
-                            : console.info('Successfully updated Notes!')
-                );
-            }
-        });
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(readNotes);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+        console.log(`Hello this is brian`)
+        // Write new notes back to the file
+        fs.writeFileSync(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+        );
 
         // Response Status
         const response = {
