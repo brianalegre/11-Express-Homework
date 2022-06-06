62a17a1ce250e466a640bf3b516e306a413adc68
Brian Alegre <brialegre@yahoo.com>
2022-06-05 21:10:30 -0700

    Fixed missing {} in htmlRoutes

--- routes/htmlRoutes..js
+++ routes/htmlRoutes..js
@@ -4,15 +4,15 @@ const router = require('express').Router()
 
 // HTML Routes
 // GET Route for notes.html
-router.get('/notes', (req, res) =>
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
-)
+})
 
 // GET Route for index.html
 // Needs to be last
-router.get('*', (req, res) =>
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
-)
+})
 
 // Export module
 module.exports = router
